6e62859ca3b7cc1ad880acbacf26a79344016ca2
DESKTOP-U2P2DON\KUCK_SU <kuck1106@naver.com>
2020-11-02 20:16:10 +0900

    fix clustering

--- MapAPI.js
+++ MapAPI.js
@@ -1,4 +1,3 @@
-
 //<script src="MarkerClustering.js"></script>
 
 
@@ -254,11 +253,13 @@ return markerOptions;
 
 naver.maps.Event.addListener(map, 'zoom_changed', function() {
     updateMarkers(map, markers,'cctv');
+   
 });
 
 
 naver.maps.Event.addListener(map, 'dragend', function() {  
     updateMarkers(map, markers,'cctv');
+
 });
 
 
@@ -630,70 +631,60 @@ naver.maps.Event.once(map, 'init_stylemap', initGeocoder);
 
 //----------------------------------------------------------------------------------------------------------------------
 
-$(document).ready(function(){
 
+	   var htmlMarker1 = {
+	            content: '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(/cluster-marker-2.png);background-size:contain;"></div>',
+	            size: N.Size(40, 40),
+	            anchor: N.Point(20, 20)
+	        },
+	        htmlMarker2 = {
+	            content: '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(/cluster-marker-2.png);background-size:contain;"></div>',
+	            size: N.Size(40, 40),
+	            anchor: N.Point(20, 20)
+	        },
+	        htmlMarker3 = {
+	            content: '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(/cluster-marker-2.png);background-size:contain;"></div>',
+	            size: N.Size(40, 40),
+	            anchor: N.Point(20, 20)
+	        },
+	        htmlMarker4 = {
+	            content: '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(/cluster-marker-2.png);background-size:contain;"></div>',
+	            size: N.Size(40, 40),
+	            anchor: N.Point(20, 20)
+	        },
+	        htmlMarker5 = {
+	            content: '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(/cluster-marker-2.png);background-size:contain;"></div>',
+	            size: N.Size(40, 40),
+	            anchor: N.Point(20, 20)
+	        };
 
-  var htmlMarker1 = {
-    content: '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url('+ HOME_PATH +'/cluster-marker-2.png);background-size:contain;"></div>',
-    size: N.Size(40, 40),
-    anchor: N.Point(20, 20)
-  },
-  htmlMarker2 = {
-    content: '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url('+ HOME_PATH +'/cluster-marker-2.png);background-size:contain;"></div>',
-    size: N.Size(40, 40),
-    anchor: N.Point(20, 20)
-  },
-  htmlMarker3 = {
-    content: '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url('+ HOME_PATH +'/cluster-marker-2.png);background-size:contain;"></div>',
-    size: N.Size(40, 40),
-    anchor: N.Point(20, 20)
-  },
-  htmlMarker4 = {
-    content: '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url('+ HOME_PATH +'/cluster-marker-2.png);background-size:contain;"></div>',
-    size: N.Size(40, 40),
-    anchor: N.Point(20, 20)
-  },
-  htmlMarker5 = {
-    content: '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url('+ HOME_PATH +'/cluster-marker-2.png);background-size:contain;"></div>',
-    size: N.Size(40, 40),
-    anchor: N.Point(20, 20)
-  };
+	    var markerClustering = new MarkerClustering({
+	        minClusterSize: 2,
+	        maxZoom: 17,
+	        map: map,
+	        markers: markers,
+	        disableClickZoom: false,
+	        gridSize: 120,
+	        icons: [htmlMarker1, htmlMarker2, htmlMarker3, htmlMarker4, htmlMarker5],
+	        indexGenerator: [10, 100, 200, 500, 1000],
+	        stylingFunction: function(clusterMarker, count) {
+	            $(clusterMarker.getElement()).find('div:first-child').text(count);
+	        }
+      });
+      
 
-  function onLoad() {
-  var data = accidentDeath.searchResult.accidentDeath;
-
-  for (var i = 0, ii = data.length; i < ii; i++) {
-    var spot = data[i],
-        latlng = new naver.maps.LatLng(spot.grd_la, spot.grd_lo),
-        marker = new naver.maps.Marker({
-            position: latlng,
-            draggable: true
-        });
-
-    markers.push(marker);
-  }
-
-  var markerClustering = new MarkerClustering({
-    minClusterSize: 2,
-    maxZoom: 8,
-    map: map,
-    markers: markers,
-    disableClickZoom: false,
-    gridSize: 120,
-    icons: [htmlMarker1, htmlMarker2, htmlMarker3, htmlMarker4, htmlMarker5],
-    indexGenerator: [10, 100, 200, 500, 1000],
-    stylingFunction: function(clusterMarker, count) {
-        $(clusterMarker.getElement()).find('div:first-child').text(count);
-    }
-  });
-
-
-
-  
-  }
-
-
-});
-
-
+	 var lightClustering = new MarkerClustering({
+	        minClusterSize: 2,
+	        maxZoom: 16,
+	        map: map,
+	        markers: markers_Light,
+	        disableClickZoom: false,
+	        gridSize: 120,
+	        icons: [htmlMarker1, htmlMarker2, htmlMarker3, htmlMarker4, htmlMarker5],
+	        indexGenerator: [10, 100, 200, 500, 1000],
+	        stylingFunction: function(clusterMarker, count) {
+	            $(clusterMarker.getElement()).find('div:first-child').text(count);
+	        }
+	    });
+    
 
